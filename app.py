63bc30d9f2cb2db31aef15452d2e2ec779bd05d3
theapk <50340965+theapk@users.noreply.github.com>
2023-06-15 14:37:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    command = db.Column(db.String(120), nullable=False)
+    schedule = db.Column(db.String(120), nullable=False)
+    last_run = db.Column(db.DateTime, nullable=True)
+    status = db.Column(db.String(20), nullable=True)
+
 def is_process_running(process_name):
     try:
         output = subprocess.check_output('pgrep -fl ' + process_name, shell=True)
