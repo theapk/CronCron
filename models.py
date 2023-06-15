c0b12fa36e4ea32fe16007e65a9de52ef52ba824
theapk <50340965+theapk@users.noreply.github.com>
2023-06-15 14:22:25 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    command = db.Column(db.String(120), nullable=False)
+    schedule = db.Column(db.String(120), nullable=False)
+    last_run = db.Column(db.DateTime, nullable=True)
+    status = db.Column(db.String(20), nullable=True)
