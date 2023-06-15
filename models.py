6906d657398e47adaa85182a07b55206e11c0f90
theapk <50340965+theapk@users.noreply.github.com>
2023-06-15 14:37:35 -0500

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-
-class Job(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    command = db.Column(db.String(120), nullable=False)
-    schedule = db.Column(db.String(120), nullable=False)
-    last_run = db.Column(db.DateTime, nullable=True)
-    status = db.Column(db.String(20), nullable=True)
